6391f3db4f496afac8d18d476bbbc5a4c5561ca5
František Milt <frantisek.milt@gmail.com>
2015-06-16 00:06:14 +0200

    Updated building scripts (added Plugin Installer)

--- Scripts/Clear Tools builds.bat
+++ Scripts/Clear Tools builds.bat
@@ -3,3 +3,9 @@
 del ..\Tools\GamesDataConverter\GamesDataConverter.exe /S /Q
 del ..\Tools\RegistryCleaner\RegistryCleaner.exe /S /Q
 del ..\Tools\SplashPreprocessor\SplashPreprocessor.exe /S /Q
+
+del ..\Tools\PluginInstaller\PluginInstaller.exe /S /Q  
+del ..\Tools\PluginInstaller\ExternalTester\Debug\win_x64\ExternalTester.exe /S /Q
+del ..\Tools\PluginInstaller\ExternalTester\Debug\win_x86\ExternalTester.exe /S /Q
+del ..\Tools\PluginInstaller\ExternalTester\Release\win_x64\ExternalTester.exe /S /Q
+del ..\Tools\PluginInstaller\ExternalTester\Release\win_x86\ExternalTester.exe /S /Q
