56206c2850a99cc151985ce9db5e524c034d5ac0
František Milt <frantisek.milt@gmail.com>
2015-08-10 00:53:57 +0200

    Added correction update for ETS2 1.19.x

--- MainProgram/Data/Updates/convert_bin.bat
+++ MainProgram/Data/Updates/convert_bin.bat
@@ -3,3 +3,4 @@ start ..\..\..\Tools\GamesDataConverter\GamesDataConverter.exe -i 2015-05-27-00.
 start ..\..\..\Tools\GamesDataConverter\GamesDataConverter.exe -i 2015-06-05-00.ini -o 2015-06-05-00.ugdb -of bin1
 start ..\..\..\Tools\GamesDataConverter\GamesDataConverter.exe -i 2015-07-27-00.ini -o 2015-07-27-00.ugdb -of bin1
 start ..\..\..\Tools\GamesDataConverter\GamesDataConverter.exe -i 2015-08-03-00.ini -o 2015-08-03-00.ugdb -of bin1
+start ..\..\..\Tools\GamesDataConverter\GamesDataConverter.exe -i 2015-08-10-00.ini -o 2015-08-10-00.ugdb -of bin1
