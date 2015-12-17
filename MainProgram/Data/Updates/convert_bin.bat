e3bb551c27e63e15c442eb6fc31369bb1992d642
František Milt <frantisek.milt@gmail.com>
2015-12-17 22:24:27 +0100

    Added support for ETS2 1.22.2

--- MainProgram/Data/Updates/convert_bin.bat
+++ MainProgram/Data/Updates/convert_bin.bat
@@ -6,3 +6,4 @@ start ..\..\..\Tools\GamesDataConverter\GamesDataConverter.exe -i 2015-08-03-00.
 start ..\..\..\Tools\GamesDataConverter\GamesDataConverter.exe -i 2015-08-10-00.ini -o 2015-08-10-00.ugdb -of bin1
 start ..\..\..\Tools\GamesDataConverter\GamesDataConverter.exe -i 2015-09-14-00.ini -o 2015-09-14-00.ugdb -of bin1
 start ..\..\..\Tools\GamesDataConverter\GamesDataConverter.exe -i 2015-10-07-00.ini -o 2015-10-07-00.ugdb -of bin1
+start ..\..\..\Tools\GamesDataConverter\GamesDataConverter.exe -i 2015-12-17-00.ini -o 2015-12-17-00.ugdb -of bin1
