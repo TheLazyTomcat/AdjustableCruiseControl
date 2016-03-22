070a78ac32acf396d90b568b72e5fcccba3c5d7a
František Milt <frantisek.milt@gmail.com>
2016-03-22 10:38:48 +0100

    Updated telemetry headers (SDK 1.6)

--- Source/Libs/SCS_Telemetry_Condensed.pas
+++ Source/Libs/SCS_Telemetry_Condensed.pas
@@ -1,8 +1,15 @@
+{-------------------------------------------------------------------------------
+
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+-------------------------------------------------------------------------------}
 unit SCS_Telemetry_Condensed;
 
 {==============================================================================}
 {  SCS Telemetry API headers condenser, version 1.0a                           }
-{  Condensed on: Saturday 2016-03-12 13:42:22                                  }
+{  Condensed on: Tuesday 2016-03-22 07:04:29                                   }
 {==============================================================================}
 
 interface
@@ -52,7 +59,13 @@ type
 {$ELSE}
   {$MESSAGE FATAL 'Cannot declare 32bit integers'}
 {$IFEND}
-  p_scs_s32_t     = ^scs_s32_t;       p_scs_u32_t     = ^scs_u32_t;
+  p_scs_s32_t   = ^scs_s32_t;         p_scs_u32_t     = ^scs_u32_t;
+
+{$IF (defined(DCC) or declared(CompilerVersion)) and not defined(FPC)}
+  {$IF (CompilerVersion <= 17)}
+  UInt64 = Int64;
+  {$IFEND}
+{$IFEND}
 
   scs_u64_t     = UInt64;             p_scs_u64_t     = ^scs_u64_t;
   scs_float_t   = Single;             p_scs_float_t   = ^scs_float_t;
@@ -2167,6 +2180,70 @@ const
 // scssdk_telemetry_trailer_common_channels.h are supported
 // with following exceptions and limitations as of v1.00:
 //
+// @li Rolling rotation of trailer wheels is determined from linear
+//     movement.
+// @li The pressures, temperatures and voltages are not simulated.
+//     They are very loosely approximated.
+
+// Configurations defined in scssdk_telemetry_common_configs.h are
+// supported with following exceptions and limitations as of v1.00:
+//
+// @li The localized strings are not updated when different in-game
+//     language is selected.
+
+{=== amtrucks/scssdk_ats.pas ==================================================}
+(**
+ * @file scssdk_ats.h
+ *
+ * @brief ATS specific constants.
+ *)
+
+const
+(**
+ * @brief Value used in the scs_sdk_init_params_t::game_id to identify this game.
+ *)
+  SCS_GAME_ID_ATS = TelemetryString('ats');
+
+{=== amtrucks/scssdk_telemetry_ats.pas ========================================}
+(**
+ * @file scssdk_telemetry_ats.h
+ *
+ * @brief ATS telemetry specific constants.
+ *)
+
+(**
+ * @name Value used in the scs_sdk_init_params_t::game_version
+ *
+ * Changes in the major version indicate incompatible changes (e.g. changed interpretation
+ * of the channel value). Change of major version is highly discouraged, creation of
+ * alternative channel is preferred solution if necessary.
+ * Changes in the minor version indicate compatible changes (e.g. added channel, more supported
+ * value types). Removal of channel is also compatible change however it is recommended
+ * to keep the channel with some default value.
+ *
+ * Changes:
+ * 1.00 - initial version - corresponds to 1.12 in ETS2
+ *)
+//@{
+const
+  SCS_TELEMETRY_ATS_GAME_VERSION_1_00    = (1 shl 16) or 0   {0x00010000};
+  SCS_TELEMETRY_ATS_GAME_VERSION_CURRENT = SCS_TELEMETRY_ATS_GAME_VERSION_1_00;
+//@}
+
+// Game specific units.
+//
+// @li The game uses US Dolars as internal currency provided
+//     by the telemetry unless documented otherwise.
+
+// Channels defined in scssdk_telemetry_common_channels.h,
+// scssdk_telemetry_truck_common_channels.h and
+// scssdk_telemetry_trailer_common_channels.h are supported
+// with following exceptions and limitations as of v1.00:
+//
+// @li Adblue related channels are not supported.
+// @li The fuel_average_consumption is currently mostly static and depends
+//     on presence of the trailer and skills of the driver instead
+//     of the workload of the engine.
 // @li Rolling rotation of trailer wheels is determined from linear
 //     movement.
 // @li The pressures, temperatures and voltages are not simulated.
